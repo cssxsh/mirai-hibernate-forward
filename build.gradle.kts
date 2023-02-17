4cca6d0ab281fca4693975961a21aa5afaaf9601
cssxsh <cssxsh@gmail.com>
2023-02-17 22:05:17 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.serialization") version "1.7.22"
+
+    id("net.mamoe.mirai-console") version "2.14.0"
+    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
+}
+
+group = "xyz.cssxsh.mirai"
+version = "0.0.1"
+
+mavenCentralPublish {
+    useCentralS01()
+    singleDevGithubProject("cssxsh", "mirai-hibernate-forward")
+    licenseFromGitHubProject("AGPL-3.0")
+    workingDir = System.getenv("PUBLICATION_TEMP")?.let { file(it).resolve(projectName) }
+        ?: buildDir.resolve("publishing-tmp")
+    publication {
+        artifact(tasks["buildPlugin"])
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.6.1")
+    testImplementation(kotlin("test"))
+    //
+    implementation(platform("net.mamoe:mirai-bom:2.14.0"))
+    testImplementation("net.mamoe:mirai-logging-slf4j")
+    //
+    implementation(platform("org.slf4j:slf4j-parent:2.0.6"))
+    testImplementation("org.slf4j:slf4j-simple")
+}
+
+mirai {
+    jvmTarget = JavaVersion.VERSION_11
+}
+
+kotlin {
+    explicitApi()
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
---
+++ src/main/kotlin/xyz/cssxsh/mirai/hibernate/forward/HibernateForwardExtension.kt
@@ -0,0 +1,57 @@
+package xyz.cssxsh.mirai.hibernate.forward
+
+import kotlinx.coroutines.cancelChildren
+import net.mamoe.mirai.console.permission.PermissionService.Companion.testPermission
+import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
+import net.mamoe.mirai.console.plugin.jvm.*
+import net.mamoe.mirai.console.util.ContactUtils.render
+import net.mamoe.mirai.event.*
+import net.mamoe.mirai.message.data.*
+import xyz.cssxsh.mirai.hibernate.*
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
+
+public object HibernateForwardExtension : KotlinPlugin(
+    JvmPluginDescription(
+        id = "xyz.cssxsh.mirai.mirai-hibernate-forward",
+        name = "mirai-hibernate-forward",
+        version = "0.0.1",
+    ) {
+        author("cssxsh")
+
+        dependsOn("xyz.cssxsh.mirai.plugin.mirai-hibernate-plugin", ">= 2.5.0")
+    }
+) {
+    private val queried: MutableMap<Long, Int> = WeakHashMap()
+
+    override fun onEnable() {
+        globalEventChannel().subscribeGroupMessages {
+            "谁@我|谁AT我".toRegex() findingReply query@{ _ ->
+                if (parentPermission.testPermission(sender.permitteeId).not()) return@query null
+                logger.info("Query At For ${sender.render()}")
+                val key = subject.id xor sender.id
+                val record = MiraiHibernateRecorder[subject].use { stream ->
+                    stream.filter { record ->
+                        val chain = record.toMessageChain()
+                        chain.findIsInstance<At>()?.target == sender.id
+                            && record.time <= queried.getOrDefault(key, Int.MAX_VALUE)
+                    }.findFirst().orElse(null)
+                } ?: return@query null
+                queried[key] = record.time
+
+                val prev = MiraiHibernateRecorder[subject, record.time - 900, record.time - 1]
+                val next = MiraiHibernateRecorder[subject, record.time, record.time + 900]
+                val records = buildList {
+                    addAll(prev.subList(0, prev.size.coerceAtMost(9)).asReversed())
+                    addAll(next.asReversed().subList(0, next.size.coerceAtMost(9)))
+                }
+
+                records.toForwardMessage(subject)
+            }
+        }
+    }
+
+    override fun onDisable() {
+        coroutineContext.cancelChildren()
+    }
+}
