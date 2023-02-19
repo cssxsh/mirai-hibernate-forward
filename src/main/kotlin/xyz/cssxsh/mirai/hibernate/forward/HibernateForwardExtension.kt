1edd17cdca506dd32ead5f179363bd2dc49bf1e5
cssxsh <cssxsh@gmail.com>
2023-02-19 14:49:17 +0800

    feat: since

--- src/main/kotlin/xyz/cssxsh/mirai/hibernate/forward/HibernateForwardExtension.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/hibernate/forward/HibernateForwardExtension.kt
@@ -5,10 +5,14 @@ import net.mamoe.mirai.console.permission.PermissionService.Companion.testPermis
 import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
 import net.mamoe.mirai.console.plugin.jvm.*
 import net.mamoe.mirai.console.util.ContactUtils.render
+import net.mamoe.mirai.contact.*
 import net.mamoe.mirai.event.*
 import net.mamoe.mirai.message.data.*
+import xyz.cssxsh.hibernate.*
 import xyz.cssxsh.mirai.hibernate.*
+import xyz.cssxsh.mirai.hibernate.entry.*
 import java.util.*
+import java.util.stream.*
 
 public object HibernateForwardExtension : KotlinPlugin(
     JvmPluginDescription(
@@ -23,17 +27,35 @@ public object HibernateForwardExtension : KotlinPlugin(
 ) {
     private val queried: MutableMap<Long, Int> = WeakHashMap()
 
+    private fun records(group: Group, since: Int): Stream<MessageRecord> {
+        val session = factory.openSession()
+        return try {
+            session.withCriteria<MessageRecord> { query ->
+                val record = query.from<MessageRecord>()
+                query.select(record)
+                    .where(
+                        equal(record.get<Int>("bot"), group.bot.id),
+                        equal(record.get<MessageSourceKind>("kind"), MessageSourceKind.GROUP),
+                        equal(record.get<Long>("targetId"), group.id),
+                        le(record.get<Int>("time"), since)
+                    )
+            }.stream().onClose { session.close() }
+        } catch (cause: Throwable) {
+            session.close()
+            throw cause
+        }
+    }
+
     override fun onEnable() {
         globalEventChannel().subscribeGroupMessages {
             "谁@我|谁AT我".toRegex() findingReply query@{ _ ->
                 if (parentPermission.testPermission(sender.permitteeId).not()) return@query null
                 logger.info("Query At For ${sender.render()}")
                 val key = subject.id xor sender.id
-                val record = MiraiHibernateRecorder[subject].use { stream ->
+                val record = records(group = subject, since = queried[key] ?: Int.MAX_VALUE).use { stream ->
                     stream.filter { record ->
                         val chain = record.toMessageChain()
                         chain.findIsInstance<At>()?.target == sender.id
-                            && record.time < queried.getOrDefault(key, Int.MAX_VALUE)
                     }.findFirst().orElse(null)
                 } ?: return@query null
                 queried[key] = record.time
