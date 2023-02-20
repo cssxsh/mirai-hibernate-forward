b43ffdd5651c51f5a2de50c6d380eb25baf3c1e2
cssxsh <cssxsh@gmail.com>
2023-02-20 18:29:27 +0800

    feat: recall

--- src/main/kotlin/xyz/cssxsh/mirai/hibernate/forward/HibernateForwardExtension.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/hibernate/forward/HibernateForwardExtension.kt
@@ -25,7 +25,8 @@ public object HibernateForwardExtension : KotlinPlugin(
         dependsOn("xyz.cssxsh.mirai.plugin.mirai-hibernate-plugin", ">= 2.5.0")
     }
 ) {
-    private val queried: MutableMap<Long, Int> = WeakHashMap()
+    private val at: MutableMap<Long, Int> = WeakHashMap()
+    private val recall: MutableMap<Long, Int> = WeakHashMap()
 
     private fun records(group: Group, since: Int): Stream<MessageRecord> {
         val session = factory.openSession()
@@ -52,13 +53,13 @@ public object HibernateForwardExtension : KotlinPlugin(
                 if (parentPermission.testPermission(sender.permitteeId).not()) return@query null
                 logger.info("Query At For ${sender.render()}")
                 val key = subject.id xor sender.id
-                val record = records(group = subject, since = queried[key] ?: Int.MAX_VALUE).use { stream ->
+                val record = records(group = subject, since = at[key] ?: Int.MAX_VALUE).use { stream ->
                     stream.filter { record ->
                         val chain = record.toMessageChain()
                         chain.findIsInstance<At>()?.target == sender.id
                     }.findFirst().orElse(null)
                 } ?: return@query null
-                queried[key] = record.time
+                at[key] = record.time
 
                 val prev = MiraiHibernateRecorder[subject, record.time - 900, record.time - 1]
                 val next = MiraiHibernateRecorder[subject, record.time, record.time + 900]
@@ -67,7 +68,28 @@ public object HibernateForwardExtension : KotlinPlugin(
                     addAll(next.asReversed().subList(0, next.size.coerceAtMost(9)))
                 }
 
-                queried[key] = records.minOfOrNull { it.time } ?: record.time
+                at[key] = records.minOfOrNull { it.time } ?: record.time
+
+                records.toForwardMessage(subject)
+            }
+            """谁撤销了""".toRegex() findingReply query@{ _ ->
+                if (parentPermission.testPermission(sender.permitteeId).not()) return@query null
+                logger.info("Query Recall For ${subject.render()}")
+                val record = records(group = subject, since = recall[subject.id] ?: Int.MAX_VALUE).use { stream ->
+                    stream.filter { record ->
+                        record.recall
+                    }.findFirst().orElse(null)
+                } ?: return@query null
+                recall[subject.id] = record.time
+
+                val prev = MiraiHibernateRecorder[subject, record.time - 900, record.time - 1]
+                val next = MiraiHibernateRecorder[subject, record.time, record.time + 900]
+                val records = buildList {
+                    addAll(prev.subList(0, prev.size.coerceAtMost(9)).asReversed())
+                    addAll(next.asReversed().subList(0, next.size.coerceAtMost(9)))
+                }
+
+                recall[subject.id] = records.minOfOrNull { it.time } ?: record.time
 
                 records.toForwardMessage(subject)
             }
